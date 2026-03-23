4c45764c01e9bf1e21a7e5bf0d4ab73357f421ce
Prince Agyei <ybktalian@gmail.com>
2026-03-23 22:45:20 +0000

    added syntax

--- If_Statements/conditional_tests.py
+++ If_Statements/conditional_tests.py
@@ -1,24 +1,35 @@
-devices = ['pc', 'laptop', 'macbook', 'phone', 'monitor', 'webcam']
+# devices = ['pc', 'laptop', 'macbook', 'phone', 'monitor', 'webcam']
+#
+# device = 'headphones'
+#
+# for _ in devices:
+#     if devices == _:
+#         print(devices)
+#     else:
+#         print("no match")
 
-device = 'headphones'
-
-for _ in devices:
-    if devices == _:
-        print(devices)
-    else:
-        print("no match")
-
-
-
+#1
 person1 = 23
 person2 = 25
 person3 = 27
 
 print(person1 > person2 or person3 > person2)
 
-
+#2
 x = 671
 b = 876
 c = 998
 
 print(c >= b and b > x)
+
+#3
+
+devices = ['pc', 'laptop', 'macbook', 'phone', 'monitor', 'webcam']
+
+u_device = 'headphones'
+
+print(u_device not in devices)
+
+
+
+
