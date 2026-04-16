930a7dd63a62736707d9e1cdd7285ce7d403f449
Prince Agyei <ybktalian@gmail.com>
2026-04-17 00:00:11 +0100

    added loop_prompt.py

---
+++ while_loops/loop+prompt.py
@@ -0,0 +1,25 @@
+from time import process_time_ns
+
+
+prompt = "\nEnter a topping for you Pizza:"
+prompt += "\ntype 'quit' when you are done\n"
+
+
+# active = True
+# while active:
+#     topping = input(prompt)
+#     if topping == 'quit':
+#         active = False
+#         print("The program ended")
+#         break
+
+prompt = "\nEnter a topping for you Pizza:"
+prompt += "\ntype 'quit' when you are done\n"
+topping = ""
+while topping != 'quit':
+    topping = input(prompt)
+    if topping == 'quit':
+        print("no more selections")
+        break
+    else:
+        print(f"{topping.title()} will be added to your Pizza")
